8faefd43d96421e9703e312f02c0353d864cadc4
AaronYorke314 <yorkeaaron872@gmail.com>
2026-05-11 14:56:09 -0600

    response

---
+++ ponder/responsive.js
@@ -0,0 +1,10 @@
+console.log('Hi');
+const menu = document.querySelector('nav');
+const btn = document.querySelector('.menu-btn');
+
+btn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    menu.classList.toggle('hide');
+    btn.classList.toggle('change');
+}
