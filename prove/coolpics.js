78f30f5b65ee84b8e6b90e11a620eff05006fd56
AaronYorke314 <yorkeaaron872@gmail.com>
2026-06-20 11:28:05 -0600

    coolpics

--- prove/coolpics.js
+++ prove/coolpics.js
@@ -1,5 +1,9 @@
 const menu = document.querySelector('nav');
+const modal = document.querySelector('dialog');
 const btn = document.querySelector('.menu-btn');
+const modalImage = modal.querySelector('.no');
+const gallery = document.querySelector('.gallery');
+const closeButton = modal.querySelector('.close-viewer');
 
 btn.addEventListener('click', toggleMenu);
 
@@ -7,3 +11,35 @@ function toggleMenu() {
     menu.classList.toggle('hide');
     btn.classList.toggle('change');
 }
+
+gallery.addEventListener('click', openModal);
+
+function openModal(e) {
+    
+// Code to show modal  - Use event parameter 'e'   
+    console.log(e.target);
+
+    const img = e.target;
+
+    const src = img.getAttribute('src');
+    const alt = img.getAttribute('alt');
+
+    const full = src.replace('sm', 'full')
+
+    modalImage.src = full;
+    modalImage.alt = alt;
+
+    modal.showModal();
+}
+// Close modal on button click
+closeButton.addEventListener('click', () => {
+    modal.close();
+});
+
+// Close modal if clicking outside the image
+modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.close();
+    }
+});
+          
